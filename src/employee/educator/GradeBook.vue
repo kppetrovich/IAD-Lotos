219c543975e2d38b580cf2f9c74ff308bd955632
Pavel <paulkur24@gmail.com>
2019-03-29 02:08:07 +0300

    Added some components

---
+++ src/employee/educator/GradeBook.vue
@@ -0,0 +1,83 @@
+<template>
+    <b-container fluid>
+
+        <!-- Main table element -->
+        <b-table
+                show-empty
+                stacked="md"
+                :items="items"
+                :fields="fields"
+        >
+            <template slot="name" slot-scope="row">
+                {{ row.value.first }} {{ row.value.last }}
+            </template>
+
+            <template slot="Attend" slot-scope="row">
+                <EditableField v-bind:value='tempValue'></EditableField>
+            </template>
+
+            <template slot="mark" slot-scope="row">
+                <EditableField v-bind:value='tempValue'></EditableField>
+            </template>
+            <template slot="eating_score" slot-scope="row">
+                <EditableField v-bind:value='tempValue'></EditableField>
+            </template>
+
+        </b-table>
+        <b-button class="saveAll" @onclick="saveEdit">Save</b-button>
+    </b-container>
+</template>
+
+<script>
+    import EditableField from "./EditableField";
+    import BButton from "bootstrap-vue/src/components/button/button";
+    const items = [
+        { Attend: true, eating_score: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
+        { Attend: false, eating_score: 21, name: { first: 'Larsen', last: 'Shaw' } },
+        {
+            Attend: false,
+            eating_score: 9,
+            name: { first: 'Mini', last: 'Navarro' },
+        },
+        { name: { first: 'Geneva', last: 'Wilson' } },
+        {  name: { first: 'Jami', last: 'Carney' } },
+        { name: { first: 'Essie', last: 'Dunlap' } },
+        { name: { first: 'Thor', last: 'Macdonald' } },
+        {name: { first: 'Larsen', last: 'Shaw' }},
+        { name: { first: 'Mitzi', last: 'Navarro' } },
+        { name: { first: 'Genevieve', last: 'Wilson' } },
+        { name: { first: 'John', last: 'Carney' } },
+        { name: { first: 'Dick', last: 'Dunlap' } }
+    ]
+
+    export default {
+        components: {BButton, EditableField},
+        data() {
+            return {
+                tempValue: '-',
+                items: items,
+                fields: [
+                    { key: 'name', label: 'Person Full name', sortable: true, sortDirection: 'desc' },
+                    { key: 'eating_score', label: 'Eating score', sortable: true, class: 'text-center' },
+                    { key: 'Attend', label: 'Attend' },
+                    { key: 'mark', label: 'Mark' }
+                ],
+            }
+        },
+        computed: {
+            sortOptions() {
+                // Create an options list from our fields
+                return this.fields
+                    .filter(f => f.sortable)
+                    .map(f => {
+                        return { text: f.label, value: f.key }
+                    })
+            }
+        },
+        methods: {
+            saveEdit: function () {
+
+            }
+        }
+    }
+</script>
---
+++ src/employee/educator/nav-bar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <b-navbar fixed="top" toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">
+                <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="BV" />
+                Lotos
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav_collapse" />
+
+            <b-collapse is-nav id="nav_collapse">
+                <b-navbar-nav>
+                    <b-nav-item href="#"></b-nav-item>
+                    <div>
+                        <b-button class="my-2 my-sm-0" @click.prevent="loadSearch">Search</b-button>
+                        <b-button class="my-2 my-sm-0" @click.prevent="loadMessages">Messages</b-button>
+                        <b-button class="my-2 my-sm-0" @click.prevent="loadGradeBook">Gradebook</b-button>
+                        <b-button class="my-2 my-sm-0" @click.prevent="loadEditGroup">Edit Group</b-button>
+                    </div>
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <b-dropdown id="ddown-dropright"  text="User"  class="m-1">
+                        <b-dropdown-item href="#"><router-link to="/BabysitterCabinet">Profile</router-link></b-dropdown-item>
+                        <b-dropdown-item  href="#"><router-link to="/login">Logout</router-link></b-dropdown-item>
+                    </b-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import BootstrapVue from 'bootstrap-vue'
+    import { EventBus } from '../../_services/event-bus.js';
+    Vue.use(BootstrapVue)
+    import BButtonToolbar from 'bootstrap-vue/es/components/button-toolbar/button-toolbar'
+    Vue.component('b-button-toolbar', BButtonToolbar)
+    export default {
+        data () {
+            return {
+            }
+        },
+        methods: {
+            loadMessages: function () {
+                EventBus.$emit('load', 'Messages');
+            },
+            loadSearch: function () {
+                EventBus.$emit('load', 'Search')
+            },
+            loadGradeBook: function () {
+                EventBus.$emit('load', 'GradeBook')
+            },
+            loadEditGroup: function () {
+                EventBus.$emit('load', 'EditGroup')
+            }
+        }
+
+    };
+</script>
+
+<style scoped>
+
+</style>
