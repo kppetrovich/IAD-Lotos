b3341c2448921590299f9e3ee982d161af5f2211
Pavel <paulkur24@gmail.com>
2019-03-29 04:08:10 +0300

    Added some components

--- src/employee/educator/EditGroup.vue
+++ src/employee/educator/EditGroup.vue
@@ -1,10 +1,46 @@
 
     <template>
-
+        <VueEditortable :data="{ columns, options }"></VueEditortable>
     </template>
 
 <script>
+    import VueEditortable from "vue-editortable"
 
+    export default {
+        data() {
+            return {
+                columns: [
+                    {
+                        title: 'Id',
+                        name: 'id',
+                        editable: false,
+                    },
+                    {
+                        title: 'Firstname',
+                        name: 'firstname',
+                        editable: true,
+                    },
+                    {
+                        title: 'Lastname',
+                        name: 'lastname',
+                        editable: true,
+                    },
+                    {
+                        title: 'Email',
+                        name: 'email',
+                        editable: true,
+                    },
+                ],
+                options: {
+                    showSearchFilter: true,
+                    requests: {
+                        allUrl: 'http://api.dev/api/users',
+                    },
+                },
+            };
+        },
+        components:{VueEditortable}
+    }
 </script>
 
 <style scoped>
